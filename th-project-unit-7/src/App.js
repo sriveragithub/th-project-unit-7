909d1c32c18e66b741744588d0d469a4ddcd8f42
Skyler Rivera <skylerrivera17@yahoo.com>
2021-08-16 09:25:04 -0400

    final change to allow loading screen on all searches

--- th-project-unit-7/src/App.js
+++ th-project-unit-7/src/App.js
@@ -26,6 +26,9 @@ class App extends Component {
   }
 
   performSearch = (query = 'dogs') => {
+    this.setState({
+      loading: true
+    })
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(res => {
         this.setState({
